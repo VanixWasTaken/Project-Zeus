5340719b311d9061f81cbae62059b26b0661a9a3
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-10-28 15:11:09 +0100

    quicksave 3

---
+++ Project Zeus/Assets/_ScenesAndScripts/Player/UnitStateMachine/UnitWalkingState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using AudioType = UnityEngine.Audio.AudioType;
+
+public class UnitWalkingState : UnitBaseState
+{
+
+    public override void EnterState(UnitStateManager _unit)
+    {
+        _unit.mAnimator.SetFloat("anSpeed", 1);
+    }
+
+    public override void UpdateState(UnitStateManager _unit)
+    {
+        // Check if the unit has reached its destination
+        if (_unit.navMeshAgent != null && !_unit.navMeshAgent.pathPending)
+        {
+            if (_unit.navMeshAgent.remainingDistance <= _unit.navMeshAgent.stoppingDistance)
+            {
+                // Stop moving and switch to idle state if the destination is reached
+                _unit.StopMoving();
+            }
+        }
+    }
+
+    public override void OnFootstep(UnitStateManager _unit)
+    {
+        _unit.audioController.RandomizeAudioPitch(AudioType.SMFootstep_01, 0.8f, 1.2f);
+    }
+}
