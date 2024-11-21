2d49ad8180c0163329e23810e807a2df7f5826cd
Sw2ft <lars-kling@gmx.net>
2024-11-21 15:14:31 +0100

    Time Limit implemented + swarm warning 1 min before end

--- Project Zeus/Assets/_ScenesAndScripts/UI/TimerUIScript.cs
+++ Project Zeus/Assets/_ScenesAndScripts/UI/TimerUIScript.cs
@@ -1,14 +1,19 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TimerUIScript : MonoBehaviour
 {
 
     TextMeshProUGUI timerText;
+    [SerializeField] GameObject swarmWarning;
     float timeElapsed = 0f;
+    bool warningFlashing = false;
 
-
+    [SerializeField] float warningTime;
+    [SerializeField] float endTime;
 
     private void Start()
     {
@@ -30,5 +35,30 @@ public class TimerUIScript : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeElapsed % 60);
 
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (minutes >= warningTime && seconds <= 1)
+        {
+            if (warningFlashing == false)
+            {
+                StartCoroutine(FlashWarning());
+            }
+        }
+        else if (minutes >= endTime)
+        {
+            StopAllCoroutines();
+            SceneManager.LoadScene("DeathScreenMenu");
+        }
+    }
+
+    IEnumerator FlashWarning()
+    {
+        warningFlashing = true;
+        while (true)
+        {
+            swarmWarning.SetActive(true);
+            yield return new WaitForSeconds(1);
+            swarmWarning.SetActive(false);
+            yield return new WaitForSeconds(1);
+        }
     }
 }
