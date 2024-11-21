287fd5f8e556ebbf58d90161763b0c31304badbd
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-11-21 13:57:59 +0100

    Loot now sets up on the gatherers back

--- Project Zeus/Assets/_ScenesAndScripts/_Features/LootingItems/Looting.cs
+++ Project Zeus/Assets/_ScenesAndScripts/_Features/LootingItems/Looting.cs
@@ -1,18 +1,25 @@
+using NUnit.Framework.Internal;
 using UnityEngine;
 
 public class Looting : MonoBehaviour
 {
 
     [SerializeField] GameObject lootGO;
+    GameObject lootOnUnit;
 
 
-    
+  
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gatherer"))
         {
-            //other.GetComponent<>
+            GathererLoot lootOnUnit = other.GetComponent<GathererLoot>();
+            
+            Debug.Log(lootOnUnit.lootGO.name);
+
+            lootOnUnit.lootGO.SetActive(true);
+
             Destroy(lootGO);
         }
     }
