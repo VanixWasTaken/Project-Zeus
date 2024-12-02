f98375ac69e50eff708f838bcb591fc4bbface73
Sw2ft <lars-kling@gmx.net>
2024-12-02 14:35:36 +0100

    quicksave

--- Project Zeus/Assets/_ScenesAndScripts/_Features/PlayableCharacters/Units/_UnitStateMachine/UnitIdleState.cs
+++ Project Zeus/Assets/_ScenesAndScripts/_Features/PlayableCharacters/Units/_UnitStateMachine/UnitIdleState.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class UnitIdleState : UnitBaseState
 {
+    #region Unity Built-In
+
     public override void EnterState(UnitStateManager _unit)
     {
         _unit.animator.SetFloat("anSpeed", 0); // plays the idle animation
@@ -11,4 +13,7 @@ public class UnitIdleState : UnitBaseState
     {
 
     }
+
+    #endregion
+
 }
